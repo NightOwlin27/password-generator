f9bf0d243613180ba817a903520926b9394d1cab
NightOwlin27 <tscotts1@yahoo.com>
2024-07-22 13:01:15 -0500

    feat: add index.html, styles.css, index.js, and README

---
+++ index.js
@@ -0,0 +1,18 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let boxOne = document.getElementsByClassName("box one")
+let boxTwo = document.getElementsByClassName("box two")
+
+
+function generatePassword () {
+    
+    for (let i = 0; i < 15; i++) {
+        let indexOne = Math.floor(Math.random() * characters.length);
+        let indexTwo = Math.floor(Math.random() * characters.length);
+        
+        boxOne.textContent += characters[indexOne];
+        boxTwo.textContent += characters[indexTwo];
+    }
+    
+}
