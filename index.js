d4857ddc3d0dcd34e380d4b98a5435c567ac113c
NightOwlin27 <144283829+NightOwlin27@users.noreply.github.com>
2024-07-25 13:31:20 -0500

    feat: add night-mode feature
    - Implemented logic and styling to create night mode feature
    - Updated HTML structure
    - Added additional styling to web page

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+
+let themeToggleBtn = document.querySelector('.theme-toggle');
 let boxOne = document.querySelector(".box.one")
 let boxTwo = document.querySelector(".box.two")
 
@@ -22,4 +24,8 @@ function generatePassword () {
     
 }
 
-// Add a night mode effect
+themeToggleBtn.addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+})
+
+
